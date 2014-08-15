57d15549e5e292ce0512a08ff6e552209e8b58da
dmitryi nizkopoklonnyi <dniz.kop@gmail.com>
2014-08-16 00:41:37 +0400

    add db

---
+++ src/com/reclame/db/DBHelper.java
@@ -0,0 +1,30 @@
+package com.reclame.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	static String LOG_TAG = "DB";
+	
+	public DBHelper(Context context) {
+		super(context, "news", null, 1);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		Log.d(LOG_TAG, "--- onCreate database ---");
+ 
+		db.execSQL("create table mytable ("
+				+ "ID integer primary key autoincrement," + "name varchar(100), description text,"
+				+ "url_picture text" + ");");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+
+}
